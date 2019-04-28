3b8de749e090b81376438c530064a4b25c1d0d2f
cog1t0 <riku4724@gmail.com>
2019-04-28 21:29:24 +0900

    修正（https://taroken.org/deploying-vue-cli-3-project-to-heroku/）

--- self-introduction/server.js
+++ self-introduction/server.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname + "/dist/"));
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+
+app = express();
+app.use(serveStatic(path.join(__dirname, "/dist")));
+
+var port = process.env.PORT || 5000;
 app.listen(port);
+
+console.log('server started '+ port);
